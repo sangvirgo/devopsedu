06afa49a50835f0dd5e0c1088e1ef886bf54c477
Sang Virgo <tansang06092004@gmail.com>
2025-04-08 23:38:25 +0700

    8.4.2025

---
+++ code/sang1.js
@@ -0,0 +1,26 @@
+function nextInLine(arr, item) {
+  // Only change code below this line
+  arr.push(item);
+  return arr.shift(item);
+  // Only change code above this line
+}
+
+// Setup
+let testArr = [1, 2, 3, 4, 5];
+
+// Display code
+
+
+
+
+
+console.log('hello');
+
+
+
+
+
+
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
+
